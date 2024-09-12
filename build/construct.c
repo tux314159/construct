67149de02cca7ec63a66b8375e39a01519b3e297
Tux <tux314159@gmail.com>
2024-09-12 21:04:01 +0800

    Idk

--- build/construct.c
+++ build/construct.c
@@ -2,6 +2,7 @@
 
 #define CC "cc"
 #define CFLAGS "-Wall -Wextra -pthread"
+#define compile_c_obj executes(l CC" "CFLAGS" -c -o" _ targ() _ firstdep() _)
 
 int
 main(int argc, char **argv)
@@ -9,14 +10,15 @@ main(int argc, char **argv)
 	construction_site(ctx);
 
 	target("testproj/main",
-			needs("testproj/main.o", "testproj/hello.o", "testproj/mymath.o"),
-			executes(l CC" "CFLAGS" -o" _ targ() _ alldeps() _));
+		needs("testproj/main.o", "testproj/hello.o", "testproj/mymath.o"),
+		executes(l CC" "CFLAGS" -o" _ targ() _ alldeps() _));
+
 	target("testproj/main.o", needs("testproj/main.c", "testproj/hello.h"),
-			executes(l CC" "CFLAGS" -c -o" _ targ() _ firstdep() _));
+		compile_c_obj);
 	target("testproj/hello.o", needs("testproj/hello.c", "testproj/hello.h"),
-			executes(l CC" "CFLAGS" -c -o" _ targ() _ firstdep() _));
+		compile_c_obj);
 	target("testproj/mymath.o", needs("testproj/mymath.c"),
-			executes(l CC" "CFLAGS" -c -o" _ targ() _ firstdep() _));
+		compile_c_obj);
 
 	construct("testproj/main", 2);
 	log(msgt_info, "build complete", 0);
